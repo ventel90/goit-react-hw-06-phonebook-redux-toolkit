07d7bcab378447dc746d0d8021a2930cb3886d48
Vitaliy Guivan <107477405+ventel90@users.noreply.github.com>
2023-02-24 10:05:15 +0200

    Create FilterContacts.jsx

--- src/components/FilterContacts/FilterContacts.jsx
+++ src/components/FilterContacts/FilterContacts.jsx
@@ -3,23 +3,18 @@ import { getFilter } from 'redux/selectors';
 import { setContactFilter } from 'redux/filterSlice';
 import { Wrapper, Label, Input } from './FilterContacts.styled';
 
-export const Filter = () => {
+export default function Filter() {
   const dispatch = useDispatch();
 //   const filter = useSelector(getFilter);
-  const contacts = useSelector(state => state.contacts);
-  const filter = useSelector(getFilter);
 
-  const changeFilter = e => {
-    console.log(e.currentTarget.value);
-    let value = e.currentTarget.value;
-    dispatch(setContactFilter(value));
-
-  };
   return (
     <Wrapper>
       <Label htmlFor="filter">Filter: </Label>``
       <Input
+        name="filter"
         type="text"
+        id="filter"
+//         value={filter}
         onChange={e => dispatch(setContactFilter(e.currentTarget.value))}
       />
     </Wrapper>
