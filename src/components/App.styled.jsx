51aa6c75beec37579ef2ca399266f0b247655ec7
VItaliy Guivan <107477405+ventel90@users.noreply.github.com>
2023-02-24 19:59:24 +0200

    v1

--- src/components/App.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-export const Container = styled.div`
-  padding: auto;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
