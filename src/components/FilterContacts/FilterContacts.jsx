2410d8bf8b85f0e0b35b6312c0d9a65a48e6ecdc
Vitaliy Guivan <107477405+ventel90@users.noreply.github.com>
2023-02-24 10:03:29 +0200

    Update FilterContacts.jsx

--- src/components/FilterContacts/FilterContacts.jsx
+++ src/components/FilterContacts/FilterContacts.jsx
@@ -6,15 +6,20 @@ import { Wrapper, Label, Input } from './FilterContacts.styled';
 export const Filter = () => {
   const dispatch = useDispatch();
 //   const filter = useSelector(getFilter);
+  const contacts = useSelector(state => state.contacts);
+  const filter = useSelector(getFilter);
 
+  const changeFilter = e => {
+    console.log(e.currentTarget.value);
+    let value = e.currentTarget.value;
+    dispatch(setContactFilter(value));
+
+  };
   return (
     <Wrapper>
       <Label htmlFor="filter">Filter: </Label>``
       <Input
-        name="filter"
         type="text"
-        id="filter"
-//         value={filter}
         onChange={e => dispatch(setContactFilter(e.currentTarget.value))}
       />
     </Wrapper>
